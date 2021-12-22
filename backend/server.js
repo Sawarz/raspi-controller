ef34f854a60eef126722b46db41221fd7242dafa
unknown <sawarz22@gmail.com>
2021-12-22 13:18:07 +0100

    Start

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const server = express()
+const server = app
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
