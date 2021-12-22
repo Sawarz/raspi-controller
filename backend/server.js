4041c14ce3b79aea209dcaedc5179b0e8086df87
unknown <sawarz22@gmail.com>
2021-12-22 23:16:11 +0100

    Start

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,16 @@ const PORT = process.env.PORT || 3000;
 const server = app
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', function(req, res) {
-  console.log("XD");
   res.send('Hello World!');
 });
+app.get('/buttondata', function(req, res) {
+  res.send('essasito');
+});
+
