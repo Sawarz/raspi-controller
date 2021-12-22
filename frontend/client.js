b6f310a87359fb9f0dfd66bbd6e163928d574565
unknown <sawarz22@gmail.com>
2021-12-22 23:02:47 +0100

    Start

--- frontend/client.js
+++ frontend/client.js
@@ -1,5 +1,5 @@
 const button = document.querySelector("#button")
-import axios from "../frontend/node_modules/axios/index";
+import axios from "../frontend/node_modules/axios/index.js";
 
 button.addEventListener("click", () => {
 axios
